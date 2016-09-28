96d0004dc0f0138d0708ebb18e5df0505eb9c430
Maison Marcel Madri Galvão dos Santos Souza <maisongalvao144@gmail.com>
2016-09-27 22:39:41 -0400

    Evento do Botão OnclickListener Toast

--- app/src/main/java/com/maiso/agenda/Formulario.java
+++ app/src/main/java/com/maiso/agenda/Formulario.java
@@ -2,6 +2,9 @@ package com.maiso.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Formulario.this,"Botão clicado",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
