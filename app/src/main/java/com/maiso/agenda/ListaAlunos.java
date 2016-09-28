9894e27e802262f846fc3f2e6305df2db6de9dbb
Maison Marcel Madri Galvão dos Santos Souza <maisongalvao144@gmail.com>
2016-09-27 21:59:03 -0400

    Adicionado ListView Métodos findviewById e Adapter

--- app/src/main/java/com/maiso/agenda/ListaAlunos.java
+++ app/src/main/java/com/maiso/agenda/ListaAlunos.java
@@ -2,6 +2,9 @@ package com.maiso.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunos extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class ListaAlunos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+        String [] alunos = {"Maison","Jose"};
+        ListView lista = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> listaadaptada = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alunos);
+        lista.setAdapter(listaadaptada);
     }
 }
