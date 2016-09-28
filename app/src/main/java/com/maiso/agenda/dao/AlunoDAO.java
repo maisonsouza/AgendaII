e7e5485ff6cf35681fa3f842facf13864110d40c
Maison Marcel Madri Galvão dos Santos Souza <maisongalvao144@gmail.com>
2016-09-28 00:59:15 -0400

    AlunoDAO

---
+++ app/src/main/java/com/maiso/agenda/dao/AlunoDAO.java
@@ -0,0 +1,33 @@
+package com.maiso.agenda.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by maiso on 28/09/2016.
+ */
+
+public class AlunoDAO extends SQLiteOpenHelper {
+
+
+    public AlunoDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, site TEXT, nota REAL);";
+        db.execSQL(sql);
+        System.out.println("Banco Criado");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS Alunos;";
+        db.execSQL(sql);
+        onCreate(db);
+
+    }
+}
